4ea6ec00f2702964f4de50eb7f9a6c79559a51bc
Jacob Mi <jacobmee@gmail.com>
2025-07-15 00:42:37 +0800

    venv mode

--- app/routes.py
+++ app/routes.py
@@ -10,4 +10,4 @@ def index():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=80)  # Bind to 0.0.0.0 for external access
+    app.run(debug=False, host="0.0.0.0", port=80)  # Bind to 0.0.0.0 for external access
